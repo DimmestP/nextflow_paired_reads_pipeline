b6fbf26e60f06a5725bdcf0cc3b34543d984b32a
Sam Haynes <dimmestp@gmail.com>
2021-10-25 15:42:24 +0100

    Fixes bug in polyA site extraction code

--- code/post-processing/find_polyA_site.sh
+++ code/post-processing/find_polyA_site.sh
@@ -1,13 +1,14 @@
-rm -r ${0}/construct_polyA_bam
+rm -r ${1}/construct_polyA_bam
 
-mkdir ${0}/construct_polyA_bam
+mkdir ${1}/construct_polyA_bam
 
-for f in ${0}/sorted_bam/*/*.bam;
+for f in ${1}/sorted_bam/*/*.bam;
 do
 	echo $f;
 	sample_name=$(expr match $f '.*\([ABE][0-9]\+\)_')
+        mkdir ${1}/construct_polyA_bam/${sample_name}/
         bedtools bamtobed -i $f |\
 	bedtools intersect -a stdin -b\
-	../data/${1}/input/genome_annotations/${sample_name}_sample_longest_full_ORF_with_constructs.gff | \
-        grep -w - | grep  'mCherry\|YNL178W\|YML028W\|YCR012W' > ${0}/construct_polyA_bam/${sample_name}/${sample_name}_polyA_reads.bed
+	../../data/${2}/input/genome_annotations/${sample_name}_sample_longest_full_ORF_with_constructs.gff | \
+        grep -w - | grep  'mCherry\|YNL178W\|YML028W\|YCR012W' > ${1}/construct_polyA_bam/${sample_name}/${sample_name}_polyA_reads.bed
 done;
