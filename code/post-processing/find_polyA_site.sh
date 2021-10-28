5045cbef9b5c00799e6fdcfa2291950e13e7f427
Sam Haynes <dimmestp@gmail.com>
2021-10-28 14:09:48 +0100

    Fixes find polyA site bug

--- code/post-processing/find_polyA_site.sh
+++ code/post-processing/find_polyA_site.sh
@@ -2,18 +2,21 @@ rm -r ${1}/construct_polyA_bam
 
 mkdir ${1}/construct_polyA_bam
 
+TMPFILE=$(mktemp) || exit 1
+
 # create regex for genes of interest
-regex='mCherry\|YNL178W\|YML028W\|YCR012W'
+regex="mCherry\|YNL178W\|YML028W\|YCR012W"
 
 for f in ${1}/sorted_bam/*/*.bam;
 do
 	echo $f;
 	sample_name=$(expr match $f '.*\([ABE][0-9]\+\)_')
-	
-	# create lamda function to filter gff files for only entries relating to genes of interest
-	gff_genes_of_interest="grep $regex ../../data/${2}/input/genome_annotations/${sample_name}_sample_longest_full_ORF_with_constructs.gff"
+
+	# create temp gff file to filter gff files for only entries relating to genes of interest
+	grep $regex ../../data/${2}/input/genome_annotations/${sample_name}_sample_longest_full_ORF_with_constructs.gff > $TMPFILE
+
         mkdir ${1}/construct_polyA_bam/${sample_name}/
         bedtools bamtobed -i $f |\
-	bedtools intersect -u -a stdin -b $gff_genes_of_interest | \
+	bedtools intersect -u -a stdin -b $TMPFILE | \
         grep -w - > ${1}/construct_polyA_bam/${sample_name}/${sample_name}_polyA_reads.bed
 done;
