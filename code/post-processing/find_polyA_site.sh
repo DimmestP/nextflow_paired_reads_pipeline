3ec933e4ba01270d3959337156d0bbf3bd283db1
Samuel Joseph Haynes <37002508+DimmestP@users.noreply.github.com>
2021-10-28 12:44:25 +0100

    Update find_polyA_site.sh

--- code/post-processing/find_polyA_site.sh
+++ code/post-processing/find_polyA_site.sh
@@ -5,13 +5,13 @@ mkdir ${1}/construct_polyA_bam
 # create regex for genes of interest
 regex='mCherry\|YNL178W\|YML028W\|YCR012W'
 
-# create lamda function to filter gff files for only entries relating to genes of interest
-gff_genes_of_interest="grep $regex ../../data/${2}/input/genome_annotations/${sample_name}_sample_longest_full_ORF_with_constructs.gff"
-
 for f in ${1}/sorted_bam/*/*.bam;
 do
 	echo $f;
 	sample_name=$(expr match $f '.*\([ABE][0-9]\+\)_')
+	
+	# create lamda function to filter gff files for only entries relating to genes of interest
+	gff_genes_of_interest="grep $regex ../../data/${2}/input/genome_annotations/${sample_name}_sample_longest_full_ORF_with_constructs.gff"
         mkdir ${1}/construct_polyA_bam/${sample_name}/
         bedtools bamtobed -i $f |\
 	bedtools intersect -u -a stdin -b $gff_genes_of_interest | \
